a77e8fec0513793706e20b249a9b39ec5fd93891
nyambogahezron <hezzyn1@gmail.com>
2025-08-26 02:34:06 +0300

    init

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
-import './global.css';
+import '../global.css';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <ScreenContent title="Home page for diary app" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
     </>
   );
